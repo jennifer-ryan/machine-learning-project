fa134d54cbf299e08e1c8454d93ba99eb7684765
jennifer-ryan <jenniferryan333@gmail.com>
2021-01-03 22:48:26 +0000

    Bare bones web app. index.html takes wind speed. predict.html prints prediction

--- power.py
+++ power.py
@@ -1,15 +1,33 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
 
 app = Flask(__name__,
             static_url_path = "", 
-            static_folder = "staticpages")
+            template_folder = "staticpages")
 
+with open("model.pkl", "rb") as mod:
+    model = pickle.load(mod)
 
 # Index
 @app.route('/')
 def index():
-    index= send_from_directory('staticpages', "index.html")
+    index= render_template("index.html")
     return index
+
+# Adapted from https://medium.com/techcrush/how-to-deploy-your-ml-model-in-jupyter-notebook-to-your-flask-app-d1c4933b29b5
+# and https://medium.com/datadriveninvestor/deployment-of-machine-learning-project-using-flask-bf6e5a750319
+# and https://towardsdatascience.com/deploying-a-trained-ml-model-using-flask-541520b3cbe9
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Get input from user
+    data = np.array(request.form["input"])
+    # change format to suit model
+    data = data.reshape(-1,1)
+    # Put data through model to make a prediction
+    prediction = model.predict(data)
+    # Send prediction to 'predict' page
+    return render_template("predict.html", data=prediction[0]) # just want the value, not the array
 
 
 # Run main
